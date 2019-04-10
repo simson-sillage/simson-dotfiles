71c37d3902abd9ceb455ef0ac24b2e48e9284de9
Simon Schricker <sschricker@suse.de>
2019-04-10 10:49:04 +0200

    Add: install script, creating symlinks

---
+++ install-configs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s -r --backup --suffix=.backup .bashrc ~/.bashrc
+ln -s -r --backup --suffix=.backup .emacs ~/.emacs
+ln -s -r --backup --suffix=.backup .screenrc ~/.screenrc
+ln -s -r --backup --suffix=.backup .tmux.conf ~/.tmux.conf
+ln -s -r --backup --suffix=.backup .gvimrc ~/.gvimrc
+ln -s -r --backup --suffix=.backup .Xresources ~/.Xresources
